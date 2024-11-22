1b54180836fe7c94b1361125d40c3d8186a64897
ranki <rankizi23@gmail.com>
2024-11-22 13:11:56 +0200

    removed old project

--- TradingSystem/TradingSystem/Position.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Position
-{
-    public string Symbol { get; set; }
-    public int Quantity { get; set; }
-    public double EntryPrice { get; set; }
-    public DateTime EntryTime { get; set; }
-    public string Side { get; set; } // "Buy" or "Sell"
-}
