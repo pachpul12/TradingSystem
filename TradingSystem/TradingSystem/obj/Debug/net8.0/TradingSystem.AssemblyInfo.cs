2fbf88cd8c253cfd0bef2a1c71b386db196821d3
ranki <rankizi23@gmail.com>
2024-11-22 13:09:20 +0200

    ddd

--- TradingSystem/TradingSystem/obj/Debug/net8.0/TradingSystem.AssemblyInfo.cs
+++ TradingSystem/TradingSystem/obj/Debug/net8.0/TradingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc9de51d155790e3fc555764d1d64558142bd72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c48c02dd4b526a09e1745d7cc4ba9d4e2844f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
